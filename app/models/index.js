a3d26158b8d5bc40cea468f7d500b24babf02975
nico_martin <mail@nicomartin.ch>
2019-12-30 08:46:48 +0100

    added retry for mngodb connect

--- app/models/index.js
+++ app/models/index.js
@@ -1,11 +1,28 @@
 import mongoose from 'mongoose';
-
+import promiseRetry from 'promise-retry';
 import User from './User';
 import Project from './Project';
 import Requests from './Requests';
 
+const options = {
+  useNewUrlParser: true,
+  reconnectTries: 60,
+  reconnectInterval: 1000,
+  poolSize: 10,
+  bufferMaxEntries: 0 // If not connected, return errors immediately rather than waiting for reconnect
+};
+
+const promiseRetryOptions = {
+  retries: options.reconnectTries,
+  factor: 2,
+  minTimeout: options.reconnectInterval,
+  maxTimeout: 5000
+};
+
 export const connectDB = () => {
-  return mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+  return promiseRetry((retry, number) => {
+    return mongoose.connect(process.env.DATABASE_URL, options).catch(retry)
+  }, promiseRetryOptions)
 };
 
 export default { User, Project, Requests };
